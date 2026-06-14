c8e2d9f13a904b2bd2555c0a395da9ec8271e647
lk5029716-boop <lk5029716-boop@users.noreply.github.com>
2026-06-14 13:56:39 +0000

    fix shell.nix - use HOME path for android sdk

--- shell.nix
+++ shell.nix
@@ -4,17 +4,14 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     jdk17
     android-tools
-    android-sdk
-    android-platform-tools
-    android-ndk
   ];
   env = {
-    ANDROID_HOME = "${pkgs.android-sdk}";
-    ANDROID_SDK_ROOT = "${pkgs.android-sdk}";
-    PATH = "${pkgs.android-sdk}/platform-tools:${pkgs.android-sdk}/emulator:${pkgs.android-tools}/bin:${pkgs.jdk17}/bin:$PATH";
+    ANDROID_HOME = "$HOME/.android-sdk";
+    ANDROID_SDK_ROOT = "$HOME/.android-sdk";
+    PATH = "$HOME/.android-sdk/platform-tools:$HOME/.android-sdk/emulator:$PATH";
   };
   shellHook = ''
-    mkdir -p $HOME/.android
-    echo "sdk.dir=${pkgs.android-sdk}" > local.properties
+    mkdir -p $HOME/.android-sdk
+    echo "sdk.dir=$HOME/.android-sdk" > local.properties
   '';
 }
