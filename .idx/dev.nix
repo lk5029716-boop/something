b32668aeb9218b13121b1c63e6bd011941bb48f6
lk5029716-boop <lk5029716-boop@users.noreply.github.com>
2026-06-14 13:50:36 +0000

    fix android sdk path in dev nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,5 +4,17 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     jdk17
     android-tools
+    android-sdk
+    android-platform-tools
+    android-ndk
   ];
+  env = {
+    ANDROID_HOME = "${pkgs.android-sdk}";
+    ANDROID_SDK_ROOT = "${pkgs.android-sdk}";
+    PATH = "${pkgs.android-sdk}/platform-tools:${pkgs.android-sdk}/emulator:${pkgs.android-tools}/bin:${pkgs.jdk17}/bin:$PATH";
+  };
+  shellHook = ''
+    mkdir -p $HOME/.android
+    echo "sdk.dir=${pkgs.android-sdk}" > local.properties
+  '';
 }
